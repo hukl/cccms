28270cfda18a18841615c57d117cbea96db1c6ec
hukl <hukl@eight.local>
2009-02-01 14:39:13 +0100

    fix for content_controller

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,12 +1,18 @@
 class ContentController < ApplicationController
 
   def render_page
-    path = params[:pagepath].join('/')
+    path = params[:page_path].join('/')
     
     @page = Node.find_page(path)
     
     # Replace with real 404
-   render :status => 404 unless @page
+    unless @page
+      render( 
+        :file => File.join(RAILS_ROOT, 'public', '404.html'),
+        :status => 404
+      )
+    end
+   
   end
 
 end
