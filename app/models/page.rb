c1854b8b518bde82b80429636f73496a537c3ad5
hukl <hukl@eight.local>
2009-02-01 21:32:44 +0100

    added first careful steps for aggregation

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,39 @@ class Page < ActiveRecord::Base
   
   acts_as_list :column => :revision, :scope => :node_id
   
+  
+  # <aggregate 
+  #   flags="updates pressemitteilungen"
+  #   path="updates/2009"
+  #   limit="20"
+  #   order_by="published_at"
+  #   order_direction="DESC"
+  # />
+  def self.aggregate options
+    
+    defaults = {
+      :flags            => "",
+      :path             => "",
+      :limit            => 20,
+      :order_by         => "id",
+      :order_direction  => "ASC"
+    }
+    
+    options = defaults.merge options
+    
+    pages = Page.all(
+      :limit => options[:limit],
+      :order => "#{options[:order_by]} #{options[:order_direction]}"
+    )
+  end
 end
+
+
+named_scope :flagged_as, lambda { |flags| 
+  conditions = {}
+  flags.each do |flag|
+    conditions[flag] = true
+  end
+  
+  { :conditions => conditions }
+}
