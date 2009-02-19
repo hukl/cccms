2075543807ba90d82aaf0c841ba962bbbc034b1c
hukl <contact@smyck.org>
2009-02-19 22:23:31 +0100

    set the default locale to :de

--- config/environment.rb
+++ config/environment.rb
@@ -37,7 +37,7 @@ Rails::Initializer.run do |config|
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
+  config.i18n.default_locale = :de
 end
 
 require 'awesome_patch'
