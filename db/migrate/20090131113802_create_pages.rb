9ebd7c278ce14e261a45fcb06c798ede7a7ba885
hukl <contact@smyck.org>
2009-02-19 21:45:49 +0100

    got the whole globalize system wrong. everything is in a translation table now. therefor merging the two separate migrations into on meaningful

--- db/migrate/20090131113802_create_pages.rb
+++ db/migrate/20090131113802_create_pages.rb
@@ -2,13 +2,12 @@ class CreatePages < ActiveRecord::Migration
   def self.up
     create_table :pages do |t|
       t.integer :node_id
-      t.string :title
-      t.text :abstract
-      t.text :body
       t.integer :revision
 
       t.timestamps
     end
+    
+    Page.create_translation_table! :title => :string, :abstract => :text, :body => :text
   end
 
   def self.down
--- db/migrate/20090207133449_add_page_translation_table.rb
+++
@@ -1,8 +0,0 @@
-class AddPageTranslationTable < ActiveRecord::Migration
-  def self.up
-      Page.create_translation_table! :title => :string, :abstract => :text, :body => :text
-    end
-    def self.down
-      Page.drop_translation_table!
-    end
-end
