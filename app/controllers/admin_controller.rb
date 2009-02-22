df13eabe5f16e35c800808ce044aa771eac02693
hukl <contact@smyck.org>
2009-02-22 22:24:45 +0100

    thats not necessary anymore

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,11 +3,5 @@ class AdminController < ApplicationController
 
   def index
   end
-
-  def switch_locale
-    I18n.locale = params[:id].to_sym
-    
-    render :controller => 'nodes', :action => 'index'
-  end
   
 end
