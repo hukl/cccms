ed5168e4c98ec7d4af6bceaa25d0a36c2777cc5d
hukl <contact@smyck.org>
2009-02-27 16:08:31 +0100

    modified the aggregate method to take only head / published pages into account

--- app/models/page.rb
+++ app/models/page.rb
@@ -21,21 +21,26 @@ class Page < ActiveRecord::Base
   # The Page.aggregate method comes with a defaults hash. These options are
   # partially or entirely overwritten by the options hash. Afterwards the merged
   # parameters are used to query the DB for Pages matching these parameters.
+  # The aggregation only takes published pages into account.
   def self.aggregate options
     
     defaults = {
       :tags             => "",
       :limit            => 20,
-      :order_by         => "id",
+      :order_by         => "pages.id",
       :order_direction  => "ASC"
     }
     
     options = defaults.merge options
     
-    pages = Page.find_tagged_with(
-      options[:tags].gsub(/\s/, ", "),
+    pages = Page.find_all_tagged_with(
+      options[:tags].gsub(/\s/, ", "), 
       :match_all => true,
-      :order => "#{options[:order_by]} #{options[:order_direction]}")
+      :order => "#{options[:order_by]} #{options[:order_direction]}",
+      :include => :node, 
+      :conditions => ["nodes.head_id = pages.id"]
+    )
+    
   end
   
   # Instance Methods
