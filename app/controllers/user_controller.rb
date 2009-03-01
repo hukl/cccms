7c272296ccb5a88fb23196c7d6a5b40ae98e3ecd
hukl <contact@smyck.org>
2009-03-01 15:33:17 +0100

    created empty user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,23 @@
+class UserController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+end
