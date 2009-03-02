ddc961fd81b37b44078fba35f0769b661e1e81ce
hukl <contact@smyck.org>
2009-03-02 14:12:23 +0100

    put the rendering of titles in the node overview into the nodes_helper

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -1,2 +1,10 @@
 module NodesHelper
+  
+  def title_for_node node
+    if node.head
+      truncate(node.head.title, :length => 50)
+    else
+      truncate(node.draft.title, :length => 50)
+    end
+  end
 end
