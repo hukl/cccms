a006c9c54bf19273fca2f0607c82c069623a4125
hukl <contact@smyck.org>
2009-03-05 21:10:43 +0100

    added gem dependencies to make the install even easier

--- config/environment.rb
+++ config/environment.rb
@@ -19,6 +19,13 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  
+  config.gem "rake",    :version => ">= 0.8.3"
+  config.gem "rack",    :version => ">= 0.9.1"
+  config.gem "mongrel", :version => ">= 1.1.5"
+  config.gem "libxml-ruby", :lib => 'xml'
+  config.gem "vpim"
+  config.gem "rcov"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
