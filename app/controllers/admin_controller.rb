d987886a9a94c6bad53ff7c7a241b85ea0b57de7
hukl <contact@smyck.org>
2009-03-10 23:48:26 +0100

    added list of drafts to admin overview

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   before_filter :login_required
 
   def index
+    @drafts = Page.find(:all, :include => [:node, :user], :conditions => ["nodes.draft_id = pages.id"])
   end
   
 end
