d3fa60e6c78f99d498be6f47ae195e35a40a5a8a
hukl <contact@smyck.org>
2009-03-21 15:00:25 +0100

    do not truncate title anymore

--- app/helpers/nodes_helper.rb
+++ app/helpers/nodes_helper.rb
@@ -2,9 +2,9 @@ module NodesHelper
   
   def title_for_node node
     if node.head
-      truncate(node.head.title, :length => 50)
+      node.head.title
     else
-      truncate(node.draft.title, :length => 50)
+      node.draft.title
     end
   end
   
