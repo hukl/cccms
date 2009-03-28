fa58bb035feba5988f259e3318d529e05db8187b
hukl <contact@smyck.org>
2009-03-28 18:10:03 +0100

    using :include => :globalize_translations here to speed up the query time

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,11 @@ class AdminController < ApplicationController
   before_filter :login_required
 
   def index
-    @drafts = Page.find(:all, :include => [:node, :user], :conditions => ["nodes.draft_id = pages.id"])
+    @drafts = Page.find(
+      :all, 
+      :include => [:node, :user, :globalize_translations], 
+      :conditions => ["nodes.draft_id = pages.id"]
+    )
   end
   
 end
