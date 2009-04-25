5a2e5f81707a0a6171733b3c26bc31b9c4cd7d7c
hukl <contact@smyck.org>
2009-04-25 14:52:28 +0200

    Paperclip patch - had to do this

--- vendor/plugins/paperclip/lib/paperclip/attachment.rb
+++ vendor/plugins/paperclip/lib/paperclip/attachment.rb
@@ -301,8 +301,7 @@ module Paperclip
     def normalize_style_definition #:nodoc:
       @styles.each do |name, args|
         unless args.is_a? Hash
-          dimensions, format = [args, nil].flatten[0..1]
-          format             = nil if format.blank?
+          dimensions, format = *args
           @styles[name]      = {
             :processors      => @processors,
             :geometry        => dimensions,
