7563dd7ef8b9bc6ba61e90976c5b5a1714230fb2
hukl <contact@smyck.org>
2009-04-27 11:19:20 +0200

    named scopes for media types

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -7,4 +7,29 @@ class Asset < ActiveRecord::Base
       :thumb => "100x100",
     }
   )
+  
+  named_scope :images, :conditions => {
+    :upload_content_type => [
+      "image/gif",
+      "image/jpeg",
+      "image/png"
+    ]
+  }
+  
+  named_scope :documents, :conditions => {
+    :upload_content_type => [
+      "application/pdf",
+      "text/plain",
+      "text/rtf"
+    ]
+  }
+  
+  named_scope :audio, :conditions => {
+    :upload_content_type => [
+      "audio/mpeg",
+      "audio/x-m4a",
+      "audio/wav",
+      "audio/x-wav"
+    ]
+  }
 end
