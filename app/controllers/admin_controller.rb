e9418758513fea6a83f63d31ecac316fdb6fc151
hukl <contact@smyck.org>
2009-04-27 16:11:47 +0200

    seperated admin overview into partials. included recent changes

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,13 @@ class AdminController < ApplicationController
 
   def index
     @drafts = Page.drafts
+    @recent_changes = Node.all(
+      :limit => 50,
+      :order => "updated_at desc",
+      :conditions => [ 
+        "updated_at < ? AND updated_at > ?", Time.now, Time.now-14.days
+      ]
+    )
   end
   
 end
