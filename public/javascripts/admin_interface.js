8235376ead33383a0009ce8993ab7a3502c38bb1
hukl <contact@smyck.org>
2009-05-07 22:51:51 +0200

    autosave only when something has actually changed. this was hard. i don't like javascript

--- public/javascripts/admin_interface.js
+++ public/javascripts/admin_interface.js
@@ -16,11 +16,4 @@ $(document).ready(function () {
     'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
   })
   
-  jQuery.fn.submitWithAjax = function() {
-    $("#flash").append("<img src='/images/ajax-loader.gif' alt='' />");
-    $.post(this.attr("action"), $(this).serialize(), null, "script");
-  };
-  
-  
-  
 });
