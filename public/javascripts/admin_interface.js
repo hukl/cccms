45e067f1108748067b727a53e7826cebbb429792
hukl <contact@smyck.org>
2009-05-08 16:30:24 +0200

    comparing content instead of string length to track changes

--- public/javascripts/admin_interface.js
+++ public/javascripts/admin_interface.js
@@ -23,37 +23,35 @@ cccms = {
   setup_autosave : function() {
     
     var elements = {
-      "title"    : $('#page_title'),
-      "abstract" : $('#page_abstract'),
-      "body"     : $('#page_body_ifr').contents().find('#tinymce'),
+      title     : $('#page_title'),
+      abstract  : $('#page_abstract'),
+      body      : $('#page_body_ifr').contents().find('#tinymce'),
     }
-  
   
     var page = {         
-      "cached_title_length"    : elements.title.val().length,
-      "cached_abstract_length" : elements.abstract.val().length,
-      "cached_body_length"     : elements.body.html().length,
+      cached_title    : elements.title.val(),
+      cached_abstract : elements.abstract.val(),
+      cached_body     : elements.body.html(),
   
-      "title_has_changed" : function() {
-         return (elements.title.val().length != this.cached_title_length)
+      title_has_changed : function() {
+         return (elements.title.val() != this.cached_title)
        },
   
-       "abstract_has_changed" : function() {
-         return (elements.abstract.val().length != this.cached_abstract_length)
+       abstract_has_changed : function() {
+         return (elements.abstract.val() != this.cached_abstract)
        },
   
-       "body_has_changed" : function() {
-         return elements.body.html().length != this.cached_body_length
+       body_has_changed : function() {
+         return elements.body.html() != this.cached_body
        }
     }
-  
   
     jQuery.fn.submitWithAjax = function(options) {
       if (page.title_has_changed() || page.abstract_has_changed() || page.body_has_changed()) {
   
-        page.cached_title_length      =  elements.title.val().length;
-        page.cached_abstract_length   = elements.abstract.val().length;
-        page.cached_body_length       = elements.body.html().length;
+        page.cached_title      = elements.title.val();
+        page.cached_abstract   = elements.abstract.val();
+        page.cached_body       = elements.body.html();
   
         $("#flash").append("<img src='/images/ajax-loader.gif' alt='' />");
         $.post(this.attr("action"), $(this).serialize(), null, "script");
