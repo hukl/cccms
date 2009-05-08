6856613c1bebf778a843eaeb4527870c5f1f72e6
hukl <contact@smyck.org>
2009-05-08 09:18:19 +0200

    put the whole autosave stuff into admin_interface.js

--- public/javascripts/admin_interface.js
+++ public/javascripts/admin_interface.js
@@ -17,3 +17,50 @@ $(document).ready(function () {
   })
   
 });
+
+
+cccms = {
+  setup_autosave : function() {
+    
+    var elements = {
+      "title"    : $('#page_title'),
+      "abstract" : $('#page_abstract'),
+      "body"     : $('#page_body_ifr').contents().find('#tinymce'),
+    }
+  
+  
+    var page = {         
+      "cached_title_length"    : elements.title.val().length,
+      "cached_abstract_length" : elements.abstract.val().length,
+      "cached_body_length"     : elements.body.html().length,
+  
+      "title_has_changed" : function() {
+         return (elements.title.val().length != this.cached_title_length)
+       },
+  
+       "abstract_has_changed" : function() {
+         return (elements.abstract.val().length != this.cached_abstract_length)
+       },
+  
+       "body_has_changed" : function() {
+         return elements.body.html().length != this.cached_body_length
+       }
+    }
+  
+  
+    jQuery.fn.submitWithAjax = function(options) {
+      if (page.title_has_changed() || page.abstract_has_changed() || page.body_has_changed()) {
+  
+        page.cached_title_length      =  elements.title.val().length;
+        page.cached_abstract_length   = elements.abstract.val().length;
+        page.cached_body_length       = elements.body.html().length;
+  
+        $("#flash").append("<img src='/images/ajax-loader.gif' alt='' />");
+        $.post(this.attr("action"), $(this).serialize(), null, "script");
+        
+      }
+    };
+  
+    setInterval('$("#page_editor > form").submitWithAjax()', 15000);
+  }
+}
