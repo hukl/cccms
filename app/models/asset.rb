e15b4259620a08b89ac390e7628c7e3081388526
hukl <contact@smyck.org>
2009-08-31 14:17:41 +0200

    added cropped headline thumbnail

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,10 +1,11 @@
 class Asset < ActiveRecord::Base
+  
   has_attached_file(
     :upload,
     :styles => {
-      :normal => "450x450", 
-      :medium => "300x300", 
-      :thumb => "100x100",
+      :medium   => "300x300",
+      :thumb    => "100x100",
+      :headline => "530x250#"
     }
   )
   
