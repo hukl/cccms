bebbe35c5b0c26d1d8f75fa98f239e41f2aee054
hukl <contact@smyck.org>
2009-11-02 22:19:20 +0100

    prevent tags internal server error

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,9 +7,12 @@ class TagsController < ApplicationController
 
   def show
     @tag    = Tag.find_by_name(params[:id])
+    
+    tag_name = @tag ? @tag.name : nil
+    
     @page   = Page.new
     @pages  = Page.heads.find_tagged_with(
-                @tag.name, :order => 'published_at DESC'
+                tag_name, :order => 'published_at DESC'
               ).paginate(
                 :page=>params[:page],
                 :per_page => 23
