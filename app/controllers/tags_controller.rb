3af611abb212052351f0979b85027adafce87a69
hukl <contact@smyck.org>
2009-12-01 23:02:39 +0100

    dramatically reduced instanciated AR objects for tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,12 +11,13 @@ class TagsController < ApplicationController
     tag_name = @tag ? @tag.name : nil
     
     @page   = Page.new
-    @pages  = Page.heads.find_tagged_with(
-                tag_name, :order => 'published_at DESC'
-              ).paginate(
-                :page=>params[:page],
-                :per_page => 23
-              )
+    @pages  = Page.heads.paginate(
+      Page.find_options_for_find_tagged_with(tag_name).merge(
+        :order => 'published_at DESC',
+        :page=>params[:page],
+        :per_page => 23
+      )
+    )
   end
 
 end
