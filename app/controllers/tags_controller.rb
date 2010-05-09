f637d999653989f9ddf240a233e88e65e1ff022f
hukl <contact@smyck.org>
2010-05-09 11:12:55 +0200

    fix for non existing tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,11 +8,12 @@ class TagsController < ApplicationController
   def show
     @tag    = Tag.find_by_name(params[:id])
     
-    tag_name = @tag ? @tag.name : nil
+    @tag = @tag ? @tag.name : params[:id]
     
     @page   = Page.new
+    
     @pages  = Page.heads.paginate(
-      Page.find_options_for_find_tagged_with(tag_name).merge(
+      Page.find_options_for_find_tagged_with(@tag).merge(
         :order => 'published_at DESC',
         :page=>params[:page],
         :per_page => 23
