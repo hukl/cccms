7379daad1c73bd3610ed296436250b417ac3673d
hukl <contact@smyck.org>
2011-02-10 14:19:00 +0100

    removed thinking_sphinx plugin and replaced it with gem. also tuned dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,10 +19,11 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  
+
   # config.gem "rake",           :version => ">= 0.8.3"
   # config.gem "rack",           :version => ">= 0.9.1"
-  config.gem "nokogiri"
+  config.gem "pg"
+  config.gem "thinking-sphinx", :lib => 'thinking_sphinx', :version => '1.4.3'
   config.gem "libxml-ruby", :lib => 'xml'
   config.gem "erdgeist-chaos_calendar", :lib => "chaos_calendar", :source => "http://gems.github.com"
 
@@ -31,16 +32,16 @@ Rails::Initializer.run do |config|
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Allowed Tags
-  # strong em b i p code pre tt samp kbd var sub sup dfn cite big small 
-  # address hr br div span h1 h2 h3 h4 h5 h6 ul ol li dt dd abbr 
+  # strong em b i p code pre tt samp kbd var sub sup dfn cite big small
+  # address hr br div span h1 h2 h3 h4 h5 h6 ul ol li dt dd abbr
   # acronym a img blockquote del ins
-  
+
   # Allowed Attributes:
   # href src width height alt cite datetime title class name xml:lang abbr))
-  
+
   # Add tags to whitelist with:
   # config.action_view.sanitized_allowed_tags = 'table', 'tr', 'td'
-  
+
   # Add attributes to whitelist with:
   # config.action_view.sanitized_allowed_attributes = 'id', 'class', 'style'
 
